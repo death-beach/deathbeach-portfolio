c6cc8cefd988ddb87df682f2911069e00bf6f678
death-beach <deathbeachstudio@gmail.com>
2026-03-03 21:29:47 -0800

    portrait down

--- pages/media.js
+++ pages/media.js
@@ -153,7 +153,7 @@ export default function PressKit() {
             height={isMobile ? 220 : 480}
             style={{
               position: "absolute",
-              top: isMobile ? "70%" : "50%",
+              top: isMobile ? "90%" : "50%",
               left: "85%",
               transform: "translate(-50%, -50%)",
               objectFit: "cover",
